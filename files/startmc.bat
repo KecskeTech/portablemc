e84ed2087d94650136ddf2b8b843521975b69be2
KecskeTech <103763612+KecskeTech@users.noreply.github.com>
2024-09-18 19:39:42 +0200

    Update startmc.bat

--- files/startmc.bat
+++ files/startmc.bat
@@ -2,7 +2,7 @@
 chcp 65001
 setlocal
 
-:: Set paths
+:variables
 set "PRISM_FILE=prism84"
 set "DOWNLOAD_URL=https://github.com/PrismLauncher/PrismLauncher/releases/download/8.4/PrismLauncher-Windows-MinGW-w64-Portable-8.4.zip"
 set "ZIP_FILE=PrismLauncher.zip"
@@ -18,7 +18,7 @@ set "JAVA_ZIP1=OpenJDK8U.zip"
 set "JAVA_ZIP2=OpenJDK17U.zip"
 set "JAVA_ZIP3=OpenJDK21U.zip"
 
-:: Check if prism84 file exists
+:checkprisminstalled
 if exist "%PRISM_FILE%" (
     echo File 'prism84' found. Skipping download...
     goto start
@@ -26,23 +26,24 @@ if exist "%PRISM_FILE%" (
     echo File 'prism84' not found. Proceeding with download...
 )
 
-:: Download the file
+:downloadprismzip
 echo Downloading file...
 bitsadmin /transfer "PrismLauncherDownload" "%DOWNLOAD_URL%" "%ZIP_FILE%"
 
-:: Unzip the downloaded file using PowerShell
+:unzipprismzip
 echo Unzipping contents...
 PowerShell -Command "Expand-Archive -Path '%ZIP_FILE%' -DestinationPath '%PRISM_FOLDER%' -Force"
 
-:: Create the marker file "prism84"
+:createmarker
 echo Creating marker file...
 echo. > "%PRISM_FILE%"
 
+:copyconfig
 copy /Y "%cd%/config.yml" "%PRISM_FOLDER%"
 copy /Y "%cd%/accounts.yml" "%PRISM_FOLDER%"
 
 
-:start
+:java
 :: Check for javaV1 marker file
 if exist "%JAVA_MARKER%" (
     echo Java environment already set up. Skipping to launch...
@@ -51,24 +52,24 @@ if exist "%JAVA_MARKER%" (
     echo Java environment not found. Proceeding with download...
 )
 
-:: Download the Java zip packages
+:javadl
 echo Downloading Java JRE packages...
 bitsadmin /transfer "JavaDownload1" "%JAVA_URL1%" "%JAVA_ZIP1%"
 bitsadmin /transfer "JavaDownload2" "%JAVA_URL2%" "%JAVA_ZIP2%"
 bitsadmin /transfer "JavaDownload3" "%JAVA_URL3%" "%JAVA_ZIP3%"
 
-:: Extract the downloaded Java zip files using PowerShell
+:javaextract
 echo Extracting Java JRE packages...
 PowerShell -Command "Expand-Archive -Path '%JAVA_ZIP1%' -DestinationPath '%DOCS_DIR%' -Force"
 PowerShell -Command "Expand-Archive -Path '%JAVA_ZIP2%' -DestinationPath '%DOCS_DIR%' -Force"
 PowerShell -Command "Expand-Archive -Path '%JAVA_ZIP3%' -DestinationPath '%DOCS_DIR%' -Force"
 
-:: Create the marker file "javaV1"
+:javamarker
 echo Creating Java setup marker file...
 echo. > "%JAVA_MARKER%"
 
-:launch
-echo Launching application...
+:start
+launch %cd%/prism /MAX prismlauncher.exe
 :: Add any further launch steps here
 
 pause
