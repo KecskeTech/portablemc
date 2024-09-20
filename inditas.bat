9745fb759b85afcba01ecc63e00825907b38c7f0
KecskeTech <103763612+KecskeTech@users.noreply.github.com>
2024-09-20 19:42:31 +0200

    +Automatic mc installscript updater/downloader

--- inditas.bat
+++ inditas.bat
@@ -10,7 +10,11 @@ echo 1) Minecraft Megnyitása
 echo 2) Beállítások Betöltése
 echo 3) Beállítások Mentése
 set /p input=.%BS%Írj be egy számot, és nyomd meg az entert! ^> 
-if /I %input% EQU 1 start startmc.bat
+if /I %input% EQU 1 (
+    mkdir files
+    powershell -Command "Invoke-WebRequest -Uri 'https://github.com/KecskeTech/portablemc/raw/refs/heads/main/files/startmc.bat' -OutFile '%cd%/files/startmc.bat'"
+    start %cd%/files/startmc.bat
+)
 if /I %input% EQU 2 start 
 if /I %input% EQU 3 start 
 timeout 1 >nul
