8768bb7e4f0c3229d894961ecf58ec6a95978fb0
KecskeTech <103763612+KecskeTech@users.noreply.github.com>
2024-09-20 19:44:57 +0200

    fix auto mcscript updater and add qol

--- inditas.bat
+++ inditas.bat
@@ -5,15 +5,15 @@ color 3
 call :banner
 :menu
 cd files
+timeout 1 >nul
 for /f %%A in ('"prompt $H &echo on &for %%B in (1) do rem"') do set BS=%%A
 echo 1) Minecraft Megnyitása
 echo 2) Beállítások Betöltése
 echo 3) Beállítások Mentése
 set /p input=.%BS%Írj be egy számot, és nyomd meg az entert! ^> 
 if /I %input% EQU 1 (
-    mkdir files
-    powershell -Command "Invoke-WebRequest -Uri 'https://github.com/KecskeTech/portablemc/raw/refs/heads/main/files/startmc.bat' -OutFile '%cd%/files/startmc.bat'"
-    start %cd%/files/startmc.bat
+    powershell -Command "Invoke-WebRequest -Uri 'https://github.com/KecskeTech/portablemc/raw/refs/heads/main/files/startmc.bat' -OutFile '%cd%/startmc.bat'"
+    start %cd%/startmc.bat
 )
 if /I %input% EQU 2 start 
 if /I %input% EQU 3 start 
